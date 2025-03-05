ca6acf0e19fd2e7f0e1340ab6d8454d3354b502d
consoli.log <eumsoli@gmail.com>
2025-03-05 18:43:10 +0900

    feat: Swagger 설정 추가
    - SwaggerConfig.java 추가하여 API 문서 설정 적용
    - application.yml에 Swagger 관련 설정 추가

---
+++ be/src/main/java/com/interviewmate/be/common/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.interviewmate.be.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * packageName  : com.interviewmate.be.common.config
+ * fileName     : SwaggerConfig
+ * author       : eumsoli
+ * date         : 2025-03-05
+ * description  : Swagger 설정
+ */
+@Configuration
+public class SwaggerConfig {
+
+    /**
+     * OpenAPI 설정을 정의하는 Bean
+     *
+     * @return OpenAPI 객체
+     */
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("인터뷰 메이트 API")
+                        .description("인터뷰 메이트 API 명세서")
+                        .version("v1.0"))
+                .servers(List.of(new Server().url("http://localhost:8080").description("로컬 서버")
+                ));
+    }
+
+}
