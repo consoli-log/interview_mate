d51a3f00efa993f5f63cea7a3814ce413020c51c
consoli.log <eumsoli@gmail.com>
2025-03-22 23:56:41 +0900

    feat: 테스트 환경 설정 추가
    - 테스트용 환경 변수 설정 추가
    - 테스트용 spring security 설정 추가

---
+++ be/src/test/java/com/interviewmate/be/common/config/EnvConfigTest.java
@@ -0,0 +1,22 @@
+package com.interviewmate.be.common.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * packageName    : com.interviewmate.be.config
+ * fileName       : EnvConfigTest
+ * author         : eumsoli
+ * date           : 2025-03-21
+ * description    : 테스트 환경에서 환경 변수 로드
+ */
+
+@Configuration
+public class EnvConfigTest {
+
+    static {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+
+}
