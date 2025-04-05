dc3cd2bd9e0e9701d879ed5bfef281dbb7b3d2f8
consoli.log <eumsoli@gmail.com>
2025-04-05 14:45:01 +0900

    docs: TEST 환경변수 설정 및 Security설정 파일명 변경

--- be/src/test/java/com/interviewmate/be/common/config/TestSecurityConfig.java
+++ be/src/test/java/com/interviewmate/be/common/config/TestSecurityConfig.java
@@ -1,7 +1,14 @@
 package com.interviewmate.be.common.config;
 
+import com.interviewmate.be.auth.application.OAuth2UserService;
+import com.interviewmate.be.auth.event.OAuth2FailureHandler;
+import com.interviewmate.be.auth.event.OAuth2SuccessHandler;
+import com.interviewmate.be.common.security.JwtAccessDeniedHandler;
+import com.interviewmate.be.common.security.JwtAuthenticationEntryPoint;
+import com.interviewmate.be.common.security.JwtTokenProvider;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -9,14 +16,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * packageName    : com.interviewmate.be.config
- * fileName       : SecurityConfigTest
+ * fileName       : TestSecurityConfig
  * author         : eumsoli
  * date           : 2025-03-21
  * description    : 테스트 환경에서 Spring Security 비활성화
  */
-@Configuration
+@TestConfiguration
 @EnableWebSecurity
-public class SecurityConfigTest {
+public class TestSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
